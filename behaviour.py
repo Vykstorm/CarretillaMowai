f8742e53417bba7d99b8955f4bf0ad1a9b62dc1b
Vykstorm <victorruizgomezdev@gmail.com>
2016-11-08 15:15:04 +0100

    Reestructurado codigo del modulo behaviour en class

--- behaviour.py
+++ behaviour.py
@@ -6,8 +6,31 @@
 
 from moway_lib import moway
 import motores, sensores
-import layer1, layer2
 
+# Esta clase representa una máquina de estados finitos.
+class DTE:
+	def __init__(self):
+		self.estado_actual = self.inicio
+	
+	# Este método modifica el estado actual.
+	def cambiar_estado(self, otro_estado):
+		print 'Cambiando estado: ' + self.estado_actual + ' -> ' + otro_estado
+		self.estado_actual = otro_estado
+
+	# Este método inicia la máquina de estados finitos.
+	def ejecutar(self):
+		while self.estado_actual != self.fin:
+			self.estado_actual()
+	
+	# Estado inicial
+	def inicio(self):
+		cambiar_estado(self.fin)
+		
+	# Representa el estado final (la máquina finaliza)
+	def fin(self):
+		pass
+	
+	
 try:
 	while True:
 		zona, direccion = layer2.behaviour()
