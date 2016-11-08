4caac2a7cd5e170c9979f37888cc8c67506021fd
Vykstorm <victorruizgomezdev@gmail.com>
2016-11-08 22:23:11 +0100

    --amend

--- behaviour.py
+++ behaviour.py
@@ -1,40 +1,119 @@
 #!/usr/bin/python
   # -*- coding: iso8859-1 -*-
 # Descripción:
-# Este módulo define el comportamiento del robot, que se divide en varias
-# capas 
+# Este módulo define el comportamiento del robot con una máquina de estados
+# finitos (para seguir una ruta específica)
 
-from moway_lib import moway
-import motores, sensores
+from sensores import get_sensores, get_sensores_discretizados
+from motores import move, girar
 
-# Esta clase representa una máquina de estados finitos.
+# Las instancias de esta clase representan  máquinas de estados finitos.
 class DTE:
 	def __init__(self):
-		self.estado_actual = self.inicio
+		pass
 	
 	# Este método modifica el estado actual.
 	def cambiar_estado(self, otro_estado):
-		print 'Cambiando estado: ' + self.estado_actual + ' -> ' + otro_estado
+		print 'Cambiando de estado: ' + self.estado_actual.__name__ + ' -> ' + otro_estado.__name__
 		self.estado_actual = otro_estado
+		
+	# Este es un método auxiliar que devolverá de forma continua las 
+	# entradas que serán pasadas como parámetros a los métodos que representan
+	# los estados.
+	def inputs(self):
+		return ()
 
 	# Este método inicia la máquina de estados finitos.
 	def ejecutar(self):
+		self.estado_actual = self.inicio
 		while self.estado_actual != self.fin:
-			self.estado_actual()
+			self.estado_actual(*self.inputs())
 	
 	# Estado inicial
-	def inicio(self):
-		cambiar_estado(self.fin)
+	def inicio(self, *args):
+		self.cambiar_estado(self.fin)
 		
 	# Representa el estado final (la máquina finaliza)
-	def fin(self):
-		pass
+	def fin(self, *args):
+		print 'Finalizando'
 	
+
+# La instancia de esta clase modelará el comportamiento del robot moway como una máquina de estados
+# finitos.
+class robot(DTE):
+	# Pasamos como parámetro la ruta que deberá llevar el robot moway 
+	def __init__(self, ruta):
+		DTE.__init__(self)
+		self.ruta = ruta
+		self.last_inputs = list(get_sensores_discretizados())
 	
-try:
-	while True:
-		zona, direccion = layer2.behaviour()
-		layer1.behaviour(zona, direccion)
-except KeyboardInterrupt:
-	print 'Interrupción del teclado. Finalizando'
-	moway.close_moway()
+	def inputs(self):
+		inputs = tuple(list(get_sensores_discretizados()) + self.last_inputs)
+		self.last_inputs = list(get_sensores_discretizados())
+		return inputs
+		
+	def cambiar_estado(self, otro_estado):
+		DTE.cambiar_estado(self, otro_estado)
+		if otro_estado == self.interseccion: 
+			self.movimiento_actual = self.ruta.siguiente_movimiento() # Siguiente movimiento ?
+			if not self.movimiento_actual:  # El robot ha alcanzado su destino
+				self.cambiar_estado(self.fin) # Finalizar la ejecución
+			else:
+				print 'Siguiente movimiento: ' + self.movimiento_actual
+	
+	# Estado inicial.
+	def inicio(self, *args):
+		self.cambiar_estado(self.pasillo)
+		
+	# Estado pasillo
+	def pasillo(self, ic, il, dc, dl, color, _ic, _il, _dc, _dl, *args):
+		if color == 2: 
+			self.cambiar_estado(self.interseccion)
+		elif (_il > 0) and (il == 0):
+			self.cambiar_estado(self.esquina_izq)
+		elif (_dl > 0) and (dl == 0):
+			self.cambiar_estado(self.esquina_der)
+		else: 
+			if (ic == 2) or (il == 2):
+				girar('right')
+			elif (dc == 2) or (dl == 2):
+				girar('left')
+			else:
+				move()	
+		
+	# Estado esquina izquierda
+	def esquina_izq(self, ic, il, dc, dl, color, *args):
+		if color == 2:
+			self.cambiar_estado(self.interseccion)
+		elif (ic == 0) and (dc == 0):
+			self.cambiar_estado(self.pasillo)
+		else:
+			if (ic == 2) or (dc == 2):
+				girar('left')
+			else:
+				move()
+				
+	# Estado esquina derecha
+	def esquina_der(self, ic, il, dc, dl, color, *args):
+		if color == 2:
+			self.cambiar_estado(self.interseccion)
+		elif (ic == 0) and (dc == 0):
+			self.cambiar_estado(self.pasillo)
+		else:
+			if (ic == 2) or (dc == 2):
+				girar('right')
+			else:
+				move()
+	
+	# Estado intersección.
+	def interseccion(self, ic, il, dc, dl, color, *args):
+		if color < 2:
+			self.cambiar_estado(self.pasillo)
+		else:
+			if self.movimiento_actual == 'left':
+				girar('left')
+			elif self.movimiento_actual == 'right':
+				girar('right')
+			elif self.movimiento_actual == 'forward':
+				move()
+
