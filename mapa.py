814a473b4b19b8f546e4aa236bfad00d1c27b34b
Vykstorm <victorruizgomezdev@gmail.com>
2016-11-21 13:11:49 +0100

    Creada estructura básica del script mapa

--- mapa.py
+++ mapa.py
@@ -4,7 +4,7 @@
 # del almacÃ©n.
 
 
-# Estructura del almacÃ©n
+# Estructura del almacén
 
 ####
 	
@@ -27,13 +27,13 @@
 
 ####
 # Zonas clave del mapa: Puntos A,B,C
-# El resto de puntos, serÃ¡n nodos intermedios que podrÃ¡ usar el algoritmo
-# de planificaciÃ³n de ruta para calcular la ruta de coste mÃ­nimo.
+# El resto de puntos, serán nodos intermedios que podrÃ¡ usar el algoritmo
+# de planificación de ruta para calcular la ruta de coste mÃ­nimo.
 # Los nodos se numeran de abajo hacia arriba y de izquierda a derecha
 # (exceptuando los nodos en el centro del tablero).
 
 
-# El centro del tablero serÃ¡ una malla de 3x3 casillas. Cada casilla es un nodo
+# El centro del tablero será una malla de 3x3 casillas. Cada casilla es un nodo
 # adicional. ...
 
 	#			N4			
@@ -46,8 +46,8 @@
 	###########		#############
 	#			N1				#
 
-# Las casillas del centro del tablero tendrÃ¡n un color asignado (para facilitar
-# la localizaciÃ³n del robot en esa zona): SerÃ¡ casillas negras o blancas
+# Las casillas del centro del tablero tendrán un color asignado (para facilitar
+# la localización del robot en esa zona): Serán casillas negras o blancas
 
 
 	#			N4			
@@ -89,26 +89,29 @@ colores[2] = [0, 1, 0]
 
 
 # Las siguientes matrices almacenan los costes de ir de un nodo a otro nodo (son usadas
-# por el algoritmo de planificaciÃ³n de ruta de coste mÃ­nimo).
-# HabrÃ¡ cuatro matrices. Cada una de ellas almacenarÃ¡ el coste de ir de un nodo a otro en 
-# funciÃ³n de su orientaciÃ³n (este, oeste,norte,sur)
+# por el algoritmo de planificación de ruta de coste mínimo).
+# Habrá cuatro matrices. Cada una de ellas almacenará el coste de ir de un nodo a otro en 
+# función de su orientación (este, oeste,norte,sur)
 
 # El coste se mide en unidades: El coste de ir de un nodo a otro es la distancia manhattan
-# entre ambos (midiendo la distancia en tÃ©rminos de casillas y no en cm)
-
-# Los giros para tomar una esquina en los pasillos, tendrÃ¡n una penalizaciÃ³n...
-coste_giro_esquina = 3
-# Los giros de 90Âº en las intersecciones y en el centro del tablero tendrÃ¡n otra...
-coste_giro_nodos = 2
+# entre ambos (midiendo la distancia en términos de casillas y no en cm)
 
 # Nodos del mapa
 nodos = ['A', 'B', 'C', 'N1', 'N2', 'N3', 'N4', 'N5','X11', 'X12', 'X13', 'X21', 'X22', 'X23', 'X31', 'X32', 'X33']
+
+# Posibles orientaciones del robot.
+orientaciones = ['este', 'oeste', 'norte', 'sur']
+
+# Posibles movimientos del robot 
+# (por ejemplo 'left' significa girar a la izquierda del nodo actual y seguir recto)
+acciones = ['left', 'right', 'forward', 'backward'] 
+
 # Casillas de los nodos del mapa (usando A como punto de origen, el este como eje X y el norte como eje Y)
 casillas = {'A':[0,0], 'B':[0,2], 'C':[5,5], 'N1':[2,1], 'N4':[2,5], 'N2':[0,3], 'N3':[4,3], 'N5':[5,3]}
 casillas.update([('X'+str(i+1)+str(j+1),[2+j,4-i]) for i in range(0,3) for j in range(0,3)])
 
 
-# Este es un mÃ©todo auxiliar que nos ayudarÃ¡ a crear la matriz de costes (calcula la distancia manhattan entre
+# Este es un método auxiliar que nos ayudará a crear la matriz de costes (calcula la distancia manhattan entre
 # dos nodos)
 def manhattan(p0, p1):
 	if type(p0) == str:
@@ -118,107 +121,83 @@ def manhattan(p0, p1):
 	return sum(map(lambda v0,v1:abs(v1-v0), p0, p1))
 
 
-# DirecciÃ³n Este.
 
-##########################
-#		 N4 ->		# C  #
-#	####	####	#	 #			#			N4->	
-#	#		   # 	#	 #			###########		#############
-#	#		   #	#	 #			#	X11->	X12->	X13->	#
-# N2 ->		   #N3-> N5->#			#							#
-#			   #		 #		#N2	->	X21->	X22->	X23->	#
-#	#		   #	#	 #			#							#
-#	#		   #	#	 #			#	X31->	X32->	X33->	#
-# B	#		   #	#	 #			###########		#############
-########	####	#	 #			#			N1->			#
-#		 N1	->		#	 #
-#	#################	 #
-# A	->					 #
-##########################
 
-d = manhattan
-ge = coste_giro_esquina
-gn = coste_giro_nodos
+# El siguiente gráfo puede usarse para conocer la vecindad de cada nodo.
 
+mapa = grafo(nodos)
+mapa.connect('A', ['N1', 'N5'])
+mapa.connect('B', 'N2')
+mapa.connect('C', 'N5')
+mapa.connect('N1', ['A', 'N3', 'X32'])
+mapa.connect('N2', ['B', 'N4', 'X21'])
+mapa.connect('N3', ['N4', 'N1', 'N5'])
+mapa.connect('N4', ['N2', 'N3', 'X12'])
+mapa.connect('N5', ['N3', 'C', 'A'])
+mapa.connect('X11', ['X12', 'X21'])
+mapa.connect('X12', ['X11', 'X22', 'X13', 'N4'])
+mapa.connect('X13', ['X12', 'X23'])
+mapa.connect('X21', ['X11', 'X31', 'X22', 'N2'])
+mapa.connect('X22', ['X21', 'X12', 'X23', 'X32'])
+mapa.connect('X23', ['X13', 'X22', 'X33'])
+mapa.connect('X31', ['X21', 'X32'])
+mapa.connect('X32', ['X31', 'X22', 'X33', 'N1'])
+mapa.connect('X33', ['X32', 'X23'])
+
+
+
+# Los siguientes gráfos definen para cada orientación posible del robot
+# (este, oeste, norte, sur), el movimiento necesario para ir de un nodo a 
+# cada uno de sus vecinos. 
+
+# Orientación ESTE
+m_este = grafo(nodos)
+
+# Orientación OESTE
+m_oeste = grafo(nodos)
+
+# Orientación NORTE
+m_norte = grafo(nodos)
+
+# Orientación SUR
+m_sur = grafo(nodos)
+
+
+# Este diccionario almacena los anteriores gráfos.
+# Si estamos en el nodo A y queremos calcular el siguiente movimiento para
+# dirigirnos al nodo vecino B, dada la orientación X del robot.
+# El siguiente movimiento podrá obtenerse de la siguiente forma:
+# movimientos[X].get(A,B)
+movimientos = {'este':m_este, 'oeste':m_oeste, 'norte':m_norte, 'sur':m_sur}
+
+
+# Los siguientes gráfos definen para cada orientación posible del robot 
+# (este, oeste, norte, sur), el coste del movimiento necesario para ir de
+# un nodo a otro.
+
+# Orientación ESTE
 c_este = grafo(nodos)
-c_este.connect('A', ['N1', 'N5'], [gn + ge + d('A','N1'),  ge + d('A', 'N5')])
-c_este.connect('B', 'N2',  gn + d('B','N2'))
-c_este.connect('C', 'N5', gn + d('C','N5'))
-c_este.connect('N1', ['N3', 'X32', 'A'], [ge + d('N1', 'N3'), gn + d('N1', 'X32'), 2*gn + d('N1', 'A') + ge])
-c_este.connect('N2', ['B', 'X21', 'N4'], [gn + d('N2','B'), d('N2', 'X21'), gn + d('N2', 'N4') + ge])
-c_este.connect('N3', ['N1', 'N4', 'N5'], [gn + d('N3', 'N1') + ge, gn + d('N3','N4') + ge, d('N3', 'N5')])
-c_este.connect('N4', ['N2', 'N3', 'X12'], [2*gn + d('N4', 'N2') + ge, d('N4', 'N3') + ge, gn + d('N4','X12')]) 
-c_este.connect('N5', ['C', 'N3', 'A'], [gn + d('N5','C'), 2*gn + d('N5','N3'), gn + d('N5', 'A') + ge])
 
-c_este.connect('X11', ['X12', 'X21'], [1, gn + 1])
-c_este.connect('X12', ['X11', 'X22', 'X13', 'N4'], [2*gn + 1, gn + 1, 1, gn + 1])
-c_este.connect('X13', ['X12', 'X23'], [2*gn + 1, gn + 1])
-c_este.connect('X21', ['X11', 'X22', 'X31', 'N2'], [gn + 1, 1, gn +1, 2*gn + 1])
-c_este.connect('X22', ['X12', 'X23', 'X32', 'X21'], [gn+1, 1,gn+1,2*gn+1])
-c_este.connect('X23', ['X13', 'X22', 'X33'], [gn+1,2*gn+1,gn+1])
-c_este.connect('X31', ['X21', 'X32'], [gn+1,1])
-c_este.connect('X32', ['X31', 'X22', 'X33', 'N1'], [2*gn+1,gn+1,1,gn+1])
-c_este.connect('X33', ['X32', 'X23'], [2*gn+1, gn+1])
-
-
-
-# DirecciÃ³n Oeste.
-
-##########################
-#	 	<- N4 		#<-C #
-#	####	####	#	 #			#			N4	
-#	#		   # 	#	 #			###########		#############
-#	#		   #	#	 #			# <-X11	  <-X12	 	<-X13	#
-#<-N2		   #<-N3 <-N5#			#							#
-#			   #		 #		#N2	  <-X21	  <-X22		<-X23	#
-#	#		   #	#	 #			#							#
-#	#		   #	#	 #			# <-X31	  <-X32		<-X33	#
-# B	#		   #	#	 #			###########		#############
-########	####	#	 #			#			N1				#
-#		<-N1		#	 #
-#	#################	 #
-#<-A					 #
-##########################
-
+# Orientación OESTE
 c_oeste = grafo(nodos)
 
-
-# DirecciÃ³n Norte.
-
-##########################
-#		 N4 ->		# C  #
-#	####	####	#	 #			#			N4->	
-#	#		   # 	#	 #			###########		#############
-#	#		   #	#	 #			#	X11->	X12->	X13->	#
-# N2 ->		   #N3-> N5->#			#							#
-#			   #		 #		#N2	->	X21->	X22->	X23->	#
-#	#		   #	#	 #			#							#
-#	#		   #	#	 #			#	X31->	X32->	X33->	#
-# B	#		   #	#	 #			###########		#############
-########	####	#	 #			#			N1->			#
-#		 N1	->		#	 #
-#	#################	 #
-# A	->					 #
-##########################
-
+# Orientación NORTE
 c_norte = grafo(nodos)
 
-# DirecciÃ³n Sur.
-
-##########################
-#		 N4 ->		# C  #
-#	####	####	#	 #			#			N4->	
-#	#		   # 	#	 #			###########		#############
-#	#		   #	#	 #			#	X11->	X12->	X13->	#
-# N2 ->		   #N3-> N5->#			#							#
-#			   #		 #		#N2	->	X21->	X22->	X23->	#
-#	#		   #	#	 #			#							#
-#	#		   #	#	 #			#	X31->	X32->	X33->	#
-# B	#		   #	#	 #			###########		#############
-########	####	#	 #			#			N1->			#
-#		 N1	->		#	 #
-#	#################	 #
-# A	->					 #
-##########################
-
+# Orientación SUR
 c_sur = grafo(nodos)
+
+costes = {'este':c_este, 'oeste':c_oeste, 'norte':c_norte, 'sur':c_sur}
+
+
+
+# Código de depuración para verificar las matrices de costes y movimientos.
+if __name__ == '__main__':
+	for k,m in movimientos.iteritems():
+		if len([(A,B) for A in nodos for B in nodos if mapa.is_fully_connected(A,B) and not m.is_fully_connected(A,B)]) > 0:
+			raise Exception()
+	for k,c in costes.iteritems():
+		if len([(A,B) for A in nodos for B in nodos if mapa.is_fully_connected(A,B) and not c.is_fully_connected(A,B)]) > 0:
+			raise Exception()
+			
+	
